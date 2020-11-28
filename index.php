b0b4773816f1ff654dfd5494600aaabbb99450b3
chandraprasadraju <54360647+chandraprasadraju@users.noreply.github.com>
2020-11-28 20:34:47 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "ba5cf8055fb07c";
+$password = "3c0c9c23";
 
-phpinfo();
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
